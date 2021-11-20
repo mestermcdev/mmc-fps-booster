20930ca4800b497f3e0a2a312470a65be0ce7f30
mestermcdev <94759695+mestermcdev@users.noreply.github.com>
2021-11-20 19:48:50 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+
+title MMC Fixer
+
+pip install time
+cls
+pip install subprocess
+cls
+pip install re
+cls
+pip install json
+cls
+pip install urllib.request
+cls
+pip install Request
+cls
+pip install urlopen
+cls
+
+python "mmc_fps_booster.py"                                                                                                                        
